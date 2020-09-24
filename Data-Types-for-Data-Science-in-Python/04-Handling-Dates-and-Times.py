7b07edb43c2801282ebfc7a0c765acbaff4bbf73
Vitor Borges Jr <Vitor>
2020-09-24 20:19:47 -0300

    Create 04-Handling-Dates-and-Times.py

---
+++ Data-Types-for-Data-Science-in-Python/04-Handling-Dates-and-Times.py
@@ -0,0 +1,10 @@
+# -------------------------------------------------
+# Data Types for Data Science in Python - Handling Dates and Times
+# 24 set 2020
+# VNTBJR
+# ------------------------------------------------
+# 
+# Load packages
+library(reticulate)
+
+# There and Back Again a Date Time Journey-------------------------------------
