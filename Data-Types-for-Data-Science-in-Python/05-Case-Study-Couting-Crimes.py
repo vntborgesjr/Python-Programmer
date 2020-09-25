cf9768a034b846f054018db852c8f4586e665575
Vitor Borges Jr <Vitor>
2020-09-25 16:59:40 -0300

    Create 05-Case-Study-Couting-Crimes.py

---
+++ Data-Types-for-Data-Science-in-Python/05-Case-Study-Couting-Crimes.py
@@ -0,0 +1,6 @@
+# -------------------------------------------------
+# Data Types for Data Science in Python - Case Study - Couting Crimes
+# 25 set 2020
+# VNTBJR
+# ------------------------------------------------
+# 
