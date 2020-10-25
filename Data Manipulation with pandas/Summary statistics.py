1c66b5e50f4e01530ededa49f83af6a01efe1328
vntborgesjr <vntborgesjr@gmail.com>
2020-10-24 22:31:19 -0300

    Create Summary statistics.py

---
+++ Data Manipulation with pandas/Summary statistics.py
@@ -0,0 +1,8 @@
+# -------------------------------------------------
+# Data Manipulation with pandas - Summary statistics
+# 24 out 2020
+# VNTBJR
+# ------------------------------------------------
+# 
+# Load packages
+library(reticulate)
