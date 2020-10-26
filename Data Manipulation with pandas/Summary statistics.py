34bf60a8b7c987f1eb5e47cdcfb8d777b6e72317
vntborgesjr <vntborgesjr@gmail.com>
2020-10-26 12:10:42 -0300

    Delete Summary statistics.py

--- Data Manipulation with pandas/Summary statistics.py
+++
@@ -1,8 +0,0 @@
-# -------------------------------------------------
-# Data Manipulation with pandas - Summary statistics
-# 24 out 2020
-# VNTBJR
-# ------------------------------------------------
-# 
-# Load packages
-library(reticulate)
