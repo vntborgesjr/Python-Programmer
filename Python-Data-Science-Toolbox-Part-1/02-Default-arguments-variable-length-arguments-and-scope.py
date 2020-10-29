adeeae5cb87350fa550d6986f49c28476e94466a
vntborgesjr <vntborgesjr@gmail.com>
2020-10-29 11:55:00 -0300

    Create 02-Default-arguments-variable-length-arguments-and-scope.py

---
+++ Python-Data-Science-Toolbox-Part-1/02-Default-arguments-variable-length-arguments-and-scope.py
@@ -0,0 +1,13 @@
+####################################################
+# Python Data Science Toolbox (Part 1) - Default arguments, variable-length arguments and scope
+# 29 Out 2020
+# VNTBJR
+####################################################
+#
+# Load packages
+library(reticulate)
+
+####################################################
+# Scope and user-defined functions ------------------------------
+####################################################
+#
